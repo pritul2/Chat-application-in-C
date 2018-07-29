5f7ee80f7b0393d82ef22ec9bf2ffa65fd0404c4
pritul2 <41751718+pritul2@users.noreply.github.com>
2018-07-29 12:55:33 +0530

    Update client.c

--- client.c
+++ client.c
@@ -1,7 +1,14 @@
 #include "socket.h" //This is my own header//
+int sockfd;
+void _error(const char *str)
+{
+    perror(str);
+    sleep(1);
+    close(sockfd);
+    exit(1);
+}
 
-
-int main(int argc, char const *argv[])
+int main(int argc, char  *argv[])
 {
     int n;
     char recvline[MAXLINE + 1];
@@ -14,29 +21,34 @@ int main(int argc, char const *argv[])
     }
 
     /*Creating Internet stream socket*/
-    if (sockfd = socket(AF_INET6, SOCK_STREAM, 0) < 0)
+    if (sockfd = socket(AF_INET, SOCK_STREAM, 0) < 0)
         _error("socket");
     
     /*Initializing TCP/IP,port number , IP address of server*/
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin6_family = AF_INET6;
-    servaddr.sin6_port = htons(13);
-    if (inet_pton(AF_INET6, argv[1], &servaddr.sin6_addr) <= 0)
-        _error("inet_pton");
+    bzero(&servaddr, sizeof(servaddr));//set memory bytes to 0//
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(13);//It conver port to binary//
     
-    /*Connecting to the server*/
-    if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) < 0)
+    //inet_pton converts the ASCII to numeric IP and stores in servaddr.sin_addr//
+    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+        _error("inet_pton");
+    //inet_addr() can also be used no matters//
+    
+
+    /*Connecting to the server by TCP*/
+    if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) < 0)//We are typecasting to SA because it is generic means we supply UDP or TCP it both accept//
         _error("connect error");
 
     /*receiving data from the server*/
     while (1)
     {
         n = read(sockfd, recvline, MAXLINE);
+        //read MAXLINE bytes from the server and store in recvline//
         if (n < 0)
         _error("read error");
 
         recvline[n] = 0; /* null terminate */
-        if (fputs(recvline, stdout) == EOF)
+        if (fputs(recvline, stdout) == EOF) //Printing data in recvline//
             _error("fputs error");
     }
     return 0;
